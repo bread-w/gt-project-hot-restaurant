82401f5fe023f86deaba8e3e792c8229a6f73ea3
Brad Williams <jb.wms89@gmail.com>
2020-07-06 11:22:20 -0400

    created working waitlist/reservation script

--- server.js
+++ server.js
@@ -28,21 +28,33 @@ const tables = [
 
 const waitlist = [];
 
-
-app.get("/api/tables", function(req, res) {
+app.get("/api/tables", function (req, res) {
   return res.json(tables);
 });
 
-app.get("/api/waitlist", function(req, res) {
+app.get("/api/waitlist", function (req, res) {
   return res.json(waitlist);
 });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+
+
+app.post("/api/tables", function (req, res) {
+  const newTable = req.body;
+  console.log(newTable);
+  if (tables.length < 5) {
+    tables.push(newTable);
+  } else {
+      waitlist.push(newTable);
+  }
+  res.json(newTable);
 });
